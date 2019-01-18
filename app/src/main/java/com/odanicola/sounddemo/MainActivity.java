b5e2fcd8675db47413d3219a96e9f191bd2d0b49
Oda Aditiya Nicola <odanicola@gmail.com>
2019-01-18 14:18:31 +0700

    update adding volume manager using SeekBar

--- app/src/main/java/com/odanicola/sounddemo/MainActivity.java
+++ app/src/main/java/com/odanicola/sounddemo/MainActivity.java
@@ -1,13 +1,17 @@
 package com.odanicola.sounddemo;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mplayer;
-
+    AudioManager audioManager;
     public void doPlay(View view){
 
         mplayer.start();
@@ -26,5 +30,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mplayer = MediaPlayer.create(this, R.raw.cinderella);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(curVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                Log.i("SeekBar value: ", "" + progress);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
